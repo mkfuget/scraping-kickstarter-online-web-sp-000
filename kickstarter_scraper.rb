98abb2001aec8097ec44fa3e326612603863490f
Michael Fuget <michaelfuget@gmail.com>
2020-12-05 20:23:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,11 +7,11 @@ def create_project_hash
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    project[title.to_sym] ={
+    projects[title.to_sym] ={
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value
       :description => project.css("p.bbcard_blurb").text
       :location => project.css(".project-meta").css('a').attribute("data-location").value
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end 
   return projects
